03ccd6204596d3ee9b0d311ec839465340ef9721
zhangshijian <zsjcwy_xcode@163.com>
2018-08-24 00:17:50 +0800

    commit

--- SJNavigationBarTransitionDemo/BaseViewController.m
+++ SJNavigationBarTransitionDemo/BaseViewController.m
@@ -35,35 +35,6 @@
 }
 
 
-//- (void)viewWillAppear:(BOOL)animated
-//{
-//    [super viewWillAppear:animated];
-//
-//    NSLog(@"viewWillAppear");
-//}
-//
-//- (void)viewDidAppear:(BOOL)animated
-//{
-//    [super viewDidAppear:animated];
-//
-//    NSLog(@"viewDidAppear");
-//}
-//
-//- (void)viewWillDisappear:(BOOL)animated
-//{
-//    [super viewWillDisappear:animated];
-//
-//    NSLog(@"viewWillDisappear");
-//}
-//
-//- (void)viewDidDisappear:(BOOL)animated
-//{
-//    [super viewDidDisappear:animated];
-//
-//    NSLog(@"viewDidDisappear");
-//}
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
